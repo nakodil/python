bb4dfacc5103e5a0b039b2171b0308f40f3581cd
nakodil <57211991+nakodil@users.noreply.github.com>
2021-02-22 21:11:54 +0300

    Update one_line_conditions.py

--- one_line_conditions.py
+++ one_line_conditions.py
@@ -1,3 +1,10 @@
 power = False
+
+if power:
+    voltage = 220
+else:
+    voltage = 0
+print("полная запись:", voltage)
+
 voltage = 220 if power else 0
-print(voltage)
+print("короткая запись:", voltage)
