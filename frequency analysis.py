5eb92825cdf4261ba4c67ad5f06bd5a8c50dbf02
nakodil <57211991+nakodil@users.noreply.github.com>
2021-03-17 12:04:15 +0300

    Update frequency analysis.py

--- frequency analysis.py
+++ frequency analysis.py
@@ -1,10 +1,19 @@
 from collections import Counter
 
-file_content = open('roadside_picnic.txt', "r", encoding="utf-8")
-text = file_content.read()
+# читаем текст из файла
+source_file = open('roadside_picnic.txt', "r", encoding="utf-8")
+text = source_file.read()
 
+# создаем список
 my_list = list(text)
+# считаем повторы символов
 result = dict(Counter(my_list))
 
+# выводим результат на экран
+# ключ : значение
 for key, value in result.items():
   print(key, ':', value)
+
+# пишем результат в файл
+destination_file = open('result.txt', "w", encoding="utf-8")
+destination_file.write(str(result))
